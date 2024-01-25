da6719bd084d9ea14ee0069994002b09ec948941
Basant-Adel <basantadel99@gmail.com>
2024-01-25 18:58:51 +0000

    Log parsing 2 :D

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,41 @@
 #!/usr/bin/python3
 """Log parsing"""
 import re
+import sys
 
 
 def extract_input(input_line):
     """extract"""
     pattern = (
         r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*\[(?P<date>\d+\.\d+\.\d+\.\d+) \S+\]',
         r'\s*"(?P<request>[^"]*)"\s*',
-        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<status_code>\d+)',
         r'\s*(?P<file_size>\d+)'
     )
     info = {'status_code': 0, 'file_size': 0}
     log_format = '{}\\-{}{}{}{}\\s*'.format(*pattern)
     match = re.fullmatch(log_format, input_line)
     if match:
-        info['status_code'] = match.group('status_code')
+        info['status_code'] = int(match.group('status_code'))
         info['file_size'] = int(match.group('file_size'))
     return info
 
 
 def print_statistics(total_file_size, status_codes_stats):
     """print"""
-    print(f'File size: {total_file_size}', flush=True)
+    print(f'File size: {total_file_size}')
     for status_code, num in sorted(status_codes_stats.items()):
         if num > 0:
-            print(f'{status_code}: {num}', flush=True)
+            print(f'{status_code}: {num}')
 
 
 def update_metrics(line, total_file_size, status_codes_stats):
     """update"""
     line_info = extract_input(line)
-    status_code = line_info.get('status_code', '0')
-    status_codes_stats[status_code] += 1
+    status_code = line_info.get('status_code', 0)
+    if status_code in status_codes_stats:
+        status_codes_stats[status_code] += 1
     return total_file_size + line_info['file_size']
 
 
@@ -41,16 +43,16 @@ def run():
     """run"""
     line_num = 0
     total_file_size = 0
-    status_codes_stats = {str(code): 0 for code in [
-        200,
-        301,
-        400,
-        401,
-        403,
-        404,
-        405,
-        500
-        ]}
+    status_codes_stats = {
+            200: 0,
+            301: 0,
+            400: 0,
+            401: 0,
+            403: 0,
+            404: 0,
+            405: 0,
+            500: 0
+            }
     try:
         while True:
             line = input()
