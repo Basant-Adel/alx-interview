d6a0832ed06b10b1d592fe9bf8d619855aa29f4d
Basant-Adel <basantadel99@gmail.com>
2024-02-27 20:55:25 +0000

    Change Comes From Within :D

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module For Making Change Using Dynamic Programming """
+
+
+def makeChange(coins, total):
+    """ Different Values & Determine The Fewest Number Of Coins """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
